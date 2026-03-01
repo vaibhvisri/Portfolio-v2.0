ec83f1247d6888cdd0d7ea5a420e17552a0b4592
Vaibhvi Srivastav <115874502+vaibhvisri@users.noreply.github.com>
2026-03-01 22:41:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 function toggle(btn){
-
 let text = btn.nextElementSibling;
-
 text.classList.toggle("hidden");
-
-if(text.classList.contains("hidden")){
-btn.innerText="View Insights";
-}else{
-btn.innerText="Hide Insights";
-}
-
 }
